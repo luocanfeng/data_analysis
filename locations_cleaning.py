364955057008e60cdfd608117b114c834199d7ee
luocanfeng <luocanfeng@gmail.com>
2016-12-05 11:10:45 +0800

    locations_cleaning.py

--- locations_cleaning.py
+++ locations_cleaning.py
@@ -17,24 +17,34 @@ t0 = time.time()
 
 datafilepath = folder + '/locations.csv'
 
-cols = ['Date', 'IMEI']
-for i in range(24):
-    cols.append(['Longitude%2d'%i, 'Latitude%2d'%i])
-print cols
-outCols = ['Date', 'IMEI', 'Longitude', 'Latitude']
 
 data = pd.read_csv(datafilepath, encoding='gbk', header=None)
-print data.columns
-
-outData = data['Date', 'IMEI', 'Longitude00', 'Latitude00']
-for i in range(1, 24):
-    outData = outData.append(data['Date', 'IMEI', 'Longitude%2d'%i, 'Latitude%2d'%i])
-
+print data.head()
 t1 = time.time()
-print 'Cost %ds'%(t1 - t0)
+print 'Load data cost %ds'%(t1 - t0)
+
+
+# TODO: 取一万条数据进行分析
+subdata = data.iloc[0:10000,:]
+
+
+outdatas = []
+for i in range(24):
+    outdata = subdata.iloc[:, [0, 1, i * 2 + 2, i * 2 + 3]]
+    outdata.columns = ['Date', 'IMEI', 'Longitude', 'Latitude']
+    outdata = outdata.dropna(axis=0)
+    outdata['DateAndHour'] = outdata['Date'] * 100 + i
+    del outdata['Date']
+    outdata = outdata.ix[:, ['IMEI', 'DateAndHour', 'Longitude', 'Latitude']]
+    outdatas.append(outdata)
+outdatas = pd.concat(outdatas)
+outdatas = outdatas.sort_values(['IMEI', 'DateAndHour'], ascending=True)
+print outdatas.head()
+
 
 outputfilepath = folder + '/locations_out.csv'
-data.to_csv(outputfilepath, index=False)
+outdatas.to_csv(outputfilepath, index=False)
+
 
 t2 = time.time()
 print 'Cost %ds'%(t2 - t1)
