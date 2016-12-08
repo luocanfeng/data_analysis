507333c9d1ab906a28c207f2c7f66a1fa6dab9ff
luocanfeng <luocanfeng@gmail.com>
2016-12-08 16:19:15 +0800

    解决中文显示，不显示科学计数法等问题

--- locations_analysis.py
+++ locations_analysis.py
@@ -20,7 +20,6 @@ sample_data_size = 10000
 # Suspend the SettingWithCopyWarning
 # pd.options.mode.chained_assignment = None  # default='warn'
 
-
 start = time.time()
 
 datafilepath = folder + '/locations_out.csv'
@@ -42,7 +41,8 @@ datas = {}
 finished = False
 large=1024*1024*1024; small=-large;
 xmin,xmax,ymin,ymax,zmin,zmax=large,small,large,small,large,small
-start_date=np.datetime64(datetime.datetime(2016, 12, 12))
+start_date=np.datetime64(datetime.datetime(1970, 1, 1))
+
 reader = pd.read_csv(datafilepath, encoding='gbk', iterator=True)
 while not finished:
     chunk = reader.get_chunk(sample_data_size)
@@ -54,17 +54,18 @@ while not finished:
                     data['DateAndHour'].apply(str)
                             .apply(lambda s:s[0:4] + '-' + s[4:6] + '-'\
                                        + s[6:8] + ' ' + s[8:10]))
-            '''
-            data.loc[:, 'DateAndHour'] = data['DateAndHour'].apply(str)
-            data.loc[:, 'DateAndHour'] = data['DateAndHour'].apply(lambda s:\
-                    s[0:4] + '-' + s[4:6] + '-' + s[6:8] + ' ' + s[8:10])
-            data.loc[:, 'DateAndHour'] = pd.to_datetime(data['DateAndHour'])
-            '''
+            
+            data.columns = ['IMEIIdx', 'Hours', 'Longitude', 'Latitude']
+            data.loc[:, 'Hours'] = data['Hours'].apply(lambda x:\
+                                (x - start_date) / np.timedelta64(3600, 's'))
+            
             xmin = min(xmin, min(data['Longitude']))
             xmax = max(xmax, max(data['Longitude']))
             ymin = min(ymin, min(data['Latitude']))
             ymax = max(ymax, max(data['Latitude']))
-            start_date = min(start_date, min(data['DateAndHour']))
+            zmin = min(zmin, min(data['Hours']))
+            zmax = max(zmax, max(data['Hours']))
+            
             if IMEIIdx in datas.keys():
                 datas[IMEIIdx].append(data)
             else:
@@ -73,44 +74,32 @@ while not finished:
             finished = True
             break
 
-# compute zmin and zmax
-for IMEIIdx in randomIMEIIndexes:
-    data = datas[IMEIIdx]
-    data.columns = ['IMEIIdx', 'Hours', 'Longitude', 'Latitude']
-    data.loc[:, 'Hours'] = data['Hours'].apply(lambda x:\
-                                (x - start_date) / np.timedelta64(3600, 's'))
-    #print data.head()
-    zmin = min(zmin, min(data['Hours']))
-    zmax = max(zmax, max(data['Hours']))
-
 
 plt.rcParams['font.sans-serif'] = ['SimHei'] #用来正常显示中文标签
 plt.rcParams['axes.unicode_minus'] = False #用来正常显示负号
 figure = plt.figure(figsize=(20, 20), dpi=400)
-#for ax in figure.axes:
-#    ax.get_xaxis().get_major_formatter().set_scientific(False)
-#    ax.get_yaxis().get_major_formatter().set_scientific(False)
 
+sub = zmin; zmax -= sub; zmin -= sub
 i = 1
 for IMEIIdx in randomIMEIIndexes:
     data = datas[IMEIIdx]
+    data.loc[:, 'Hours'] = data['Hours'] - sub
     IMEI = Index2IMEIDict[IMEIIdx]
 
     ax = plt.subplot(4, 4, i, projection='3d')
-    plt.title(IMEI)
-    plt.xlim(math.floor(xmin*10)/10, math.ceil(xmax*10)/10);
-    plt.xlabel('Longitude')
-    plt.ylim(math.floor(ymin*10)/10, math.ceil(ymax*10)/10);
-    plt.ylabel('Latitude')
+    ax.ticklabel_format(style='plain', axis='both')
+    ax.set_title(u'用户%s'%IMEI)
+    ax.set_xlim(math.floor(xmin*10)/10, math.ceil(xmax*10)/10);
+    ax.set_xlabel(u'经度')
+    ax.set_ylim(math.floor(ymin*10)/10, math.ceil(ymax*10)/10);
+    ax.set_ylabel(u'纬度')
     ax.set_zlim(zmin, zmax)
-    ax.set_zlabel('Hours')
-    ax.get_xaxis().get_major_formatter().set_scientific(False)
-    ax.get_yaxis().get_major_formatter().set_scientific(False)
-    plt.plot(data['Longitude'], data['Latitude'], zs=data['Hours'], zdir='z')
+    ax.set_zlabel(u'从某刻起的小时数')
+    ax.plot(data['Longitude'], data['Latitude'], zs=data['Hours'], zdir='z')
     i = i + 1
 
-
 figure.show()
+
 figure.savefig(folder + '/test.png')
 
 
